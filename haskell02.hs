53fe64a319677259d4db238eeeec1bfebabe387f
miguelwisneski <85126042+miguelwisneski@users.noreply.github.com>
2021-06-08 10:29:20 -0300

    Update haskell02.hs

--- haskell02.hs
+++ haskell02.hs
@@ -1,2 +1,17 @@
 -- Prática 02 de Haskell
--- Nome: 
+-- Nome: Miguel Antonio Welter Wisneski
+
+--1
+testaFebre :: Float -> Bool 
+testaFebre x = x >= 37.8
+
+comFebre :: [Float] -> [Float]
+comFebre f1 = (filter (testaFebre) f1)
+
+--2
+comFebre' :: [Float] -> [Float]
+comFebre' f1 = (filter (\x -> x >= 37.8) f1)
+
+--3
+itemize :: [String] -> [String]
+itemize x = (map (\x -> "<li>" ++ "</ li>" ++ x ++ "<li>" ++ "</ li>")x)
